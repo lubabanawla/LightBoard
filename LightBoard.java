998d8d83ecf40c318c60d0b86cb694d43afcb9bb
Lubaba Nawla <lubabanawla@gmail.com>
2025-05-04 21:53:37 -0400

    Add files via upload
    Finished

--- LightBoard.java
+++ LightBoard.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LightBoard {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    /**
+     * The lights on the board, where true represents on and false represents off.
+     */
+
+    private boolean[][] lights;
+
+    /**
+     * Constructs a LightBoard object having numRows rows and numCols columns.
+     * Precondition: numRows > 0, numCols > 0
+     * Postcondition: each light has a 40% probability of being set to on.
+     */
+
+    public LightBoard(int numRows, int numCols)
+    {
+        lights = new boolean[numRows][numCols];
+        for(int r = 0; r < lights.length; r++){
+            for(int c = 0; c < lights[0].length; c++){
+                if(Math.random() < 0.4) lights[r][c] = true;
+            }
         }
     }
+
+    public double test()
+    {
+        double on = 0;
+        for(boolean[] row : lights){
+            for(boolean b : row){
+                if(b) on++;
+            }
+        }
+        return on / (lights.length * lights[0].length);
+    }
+
+    /**
+     * Evaluates a light in row index row and column index col and returns a status
+     * as described in part (b).
+     * Precondition: row and col are valid indexes in lights.
+     */
+
+    public boolean evaluateLight(int row, int col)
+    {
+        boolean light = lights[row][col];
+        int colLights = 0;
+        for(int r = 0; r < lights.length-1; r++){
+            if(lights[r][col]) colLights++;
+        }
+        if(light && colLights % 2 == 0) return false;
+        if(!light && colLights % 3 ==0) return true;
+        return light;
+    }
+
+    public void setLights(boolean[][] arr){
+        lights = arr;
+    }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
+    public static void main(String[] args){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        LightBoard l = new LightBoard(1000, 1000);
+
+        System.out.println(l.test());
+        System.out.println();
+        System.out.println();
+
+        LightBoard sim = new LightBoard(7, 5);
+
+        boolean[][] lights ={
+                {true, true, false, true, true},
+                {true, false, false, true, false},
+                {true, false, false, true, true},
+                {true, false, false, false, true},
+                {true, false, false, false, true},
+                {true, true, false, true, true},
+                {false, false, false, false}
+        };
+
+        sim.setLights(lights);
+
+        System.out.println(sim.evaluateLight(0, 3));
+        System.out.println(sim.evaluateLight(6, 0));
+        System.out.println(sim.evaluateLight(4, 1));
+        System.out.println(sim.evaluateLight(5, 4));
     }
 }
